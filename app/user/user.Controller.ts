b7bd5720dc4c82dc36832d9849602f8a48d4e872
lazlanrafar <lazlanrafar@gmail.com>
2024-06-18 14:06:22 +0800

    feat(user): fetch user detail by id

--- app/user/user.Controller.ts
+++ app/user/user.Controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from "express";
 import { InternalServerError, Ok } from "@/utils/api-response";
-import { FetchUser } from "./user.Repository";
+import { FetchUser, FetchUserById } from "./user.Repository";
 
 export const GetUser = async (req: Request, res: Response) => {
   try {
     const users = await FetchUser();
 
     return await Ok({ res, data: users });
+  } catch (error) {
+    return InternalServerError({ res, data: error });
+  }
+};
+
+export const GetUserById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const user = await FetchUserById(id);
+
+    return await Ok({ res, data: user });
   } catch (error) {
     return InternalServerError({ res, data: error });
   }
--- app/user/user.Route.ts
+++ app/user/user.Route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { GetUser } from "./user.Controller";
+import { GetUser, GetUserById } from "./user.Controller";
 
 const UserRoute = Router();
 
 UserRoute.get("/", GetUser);
+UserRoute.get("/:id", GetUserById);
 
 export default UserRoute;
